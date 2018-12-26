053b88f765b6474dccdccef5f3974b9f3bb12767
Travis CI User <travis@example.org>
2018-12-26 20:49:19 +0000

    Update documentation

---
+++ implementors/compression/prelude/trait.Encoder.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["compression"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
